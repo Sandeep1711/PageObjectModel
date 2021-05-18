8b6c61d10d1f7256fce15d92159593f028f4af2f
Sandeep1711 <38990494+Sandeep1711@users.noreply.github.com>
2021-05-18 11:27:26 +0530

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -6,23 +6,26 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.crm.qa.base.TestBase;
+
 
 public class TestUtil extends TestBase {
 
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "/Users/naveenkhunteta/Documents/workspace"
-			+ "/FreeCRMTest/src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "C:\\Users\\sandeep_sharma1\\OneDrive - S&P Global\\QA-Testing\\Java tutorials\\Java_project\\PageObjectModel\\src\\main\\java\\com\\crm\\qa\\testdata\\FreeCrmTestData.xlsx";
+
 
 	static Workbook book;
 	static Sheet sheet;
@@ -32,40 +35,58 @@ public class TestUtil extends TestBase {
 		driver.switchTo().frame("mainpanel");
 	}
 
-	public static Object[][] getTestData(String sheetName) {
-		FileInputStream file = null;
-		try {
-			file = new FileInputStream(TESTDATA_SHEET_PATH);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		try {
-			book = WorkbookFactory.create(file);
-		} catch (InvalidFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		sheet = book.getSheet(sheetName);
-		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
-		// System.out.println(sheet.getLastRowNum() + "--------" +
-		// sheet.getRow(0).getLastCellNum());
-		for (int i = 0; i < sheet.getLastRowNum(); i++) {
-			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
-				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
-				// System.out.println(data[i][k]);
-			}
-		}
-		return data;
-	}
+//	public static Object[][] getTestData(String sheetName) {
+//		FileInputStream file = null;
+//
+//			try {
+//				file = new FileInputStream(TESTDATA_SHEET_PATH);
+//			} catch (FileNotFoundException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//		
+//		try {
+//			book = WorkbookFactory.create(file);
+//		} 
+//		catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		sheet = book.getSheet(sheetName);
+//		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+//		// System.out.println(sheet.getLastRowNum() + "--------" +
+//		// sheet.getRow(0).getLastCellNum());
+//		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+//			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+//				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+//				// System.out.println(data[i][k]);
+//			}
+//		}
+//		return data;
+//	}
 
-	public static void takeScreenshotAtEndOfTest() throws IOException {
+	public void wait_for_pageload(int waitTime)
+	{
+		WebDriverWait wait = new WebDriverWait(driver, waitTime);
+		wait.until((ExpectedCondition<Boolean>) wd -> ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete")); 
+	}
+	
+	public static String getCurrent_url()
+	{
+		return driver.getCurrentUrl();
+	}
+	
+	public static String get_title_page()
+	{
+		return driver.getTitle();
+	}
+	
+	public  void takeScreenshotAtEndOfTest() throws IOException {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
 		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
 	}
 
-	public static void runTimeInfo(String messageType, String message) throws InterruptedException {
+	public  void runTimeInfo(String messageType, String message) throws InterruptedException {
 		js = (JavascriptExecutor) driver;
 		// Check for jQuery on the page, add it if need be
 		js.executeScript("if (!window.jQuery) {"
@@ -93,11 +114,7 @@ public class TestUtil extends TestBase {
 			js.executeScript("$.growl.warning({ title: 'Warning!', message: 'your warning message goes here' });");
 		}else
 			System.out.println("no error message");
-		// jquery-growl w/ colorized output
-//		js.executeScript("$.growl.error({ title: 'ERROR', message: 'your error message goes here' });");
-//		js.executeScript("$.growl.notice({ title: 'Notice', message: 'your notice message goes here' });");
-//		js.executeScript("$.growl.warning({ title: 'Warning!', message: 'your warning message goes here' });");
+
 		Thread.sleep(5000);
 	}
-
 }
