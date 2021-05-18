0323dc6192f4a7f7fcd775f763d3e87db3e46a35
Sandeep1711 <38990494+Sandeep1711@users.noreply.github.com>
2021-05-18 10:55:46 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -3,31 +3,35 @@ package com.crm.qa.base;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+
+//import com.relevantcodes.extentreports.ExtentReports;
+//import com.relevantcodes.extentreports.ExtentTest;
+//import com.relevantcodes.extentreports.LogStatus;
+
 import com.crm.qa.util.TestUtil;
-import com.crm.qa.util.WebEventListener;
 
 public class TestBase {
-	
+
 	public static WebDriver driver;
 	public static Properties prop;
-	public  static EventFiringWebDriver e_driver;
-	public static WebEventListener eventListener;
+	public static EventFiringWebDriver e_driver;
+
 	
-	public TestBase(){
+	public TestBase() {
 		try {
 			prop = new Properties();
-			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/com/crm"
-					+ "/qa/config/config.properties");
+			FileInputStream ip = new FileInputStream(
+					System.getProperty("user.dir") + "/src/main/java/com/crm" + "/qa/config/config.properties");
 			prop.load(ip);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -36,41 +40,29 @@ public class TestBase {
 		}
 	}
 	
-	
-	public static void initialization(){
-		String browserName = prop.getProperty("browser");
+
+	public void initialization() throws MalformedURLException {
+		//String browserName = prop.getProperty("browser");
 		
-		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
-			driver = new ChromeDriver(); 
+		String browserName = System.getProperty("xyz");
+		System.out.println("CMD browser name : "+browserName);
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:/Users/sandeep_sharma1/browsers/chromedriver.exe");
+			driver = new ChromeDriver();
 		}
-		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
-			driver = new FirefoxDriver(); 
+		else if(browserName.equals("remote chrome"))
+		{
+			DesiredCapabilities dc = DesiredCapabilities.chrome();			
+			URL url = new URL("http://localhost:4444/wd/hub");
+			driver = new RemoteWebDriver(url, dc);
 		}
-		
-		
-		e_driver = new EventFiringWebDriver(driver);
-		// Now create object of EventListerHandler to register it with EventFiringWebDriver
-		eventListener = new WebEventListener();
-		e_driver.register(eventListener);
-		driver = e_driver;
-		
+
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
-		
 		driver.get(prop.getProperty("url"));
 		
+		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
